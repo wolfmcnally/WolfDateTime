a7e5daf20a0b518ff30d265ecd1105797330bf93
Wolf McNally <wolf@wolfmcnally.com>
2019-09-30 00:27:35 -0700

    Set minimum MacOS version to 10.13.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "WolfDateTime",
+    platforms: [
+        .macOS(.v10_13)
+    ],
     products: [
         .library(
             name: "WolfDateTime",
